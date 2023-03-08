2e67f1cdd9b19298354a07b593bbf852df803161
Victoria Pham <victoriapham52@gmail.com>
2023-03-08 07:25:57 -0800

    added camel casing

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -9,30 +9,30 @@ namespace MethodsExercise
             //Exercise 1: Story
 
             //Console.WriteLine("What is your first name?"); //output
-            //var name = Console.ReadLine(); //input
+            //var firstName = Console.ReadLine(); //input
 
             //Console.WriteLine("What is your favorite color?"); //output
-            //var color = Console.ReadLine(); //input
+            //var faveColor = Console.ReadLine(); //input
 
             //Console.WriteLine("What is an item of clothing?"); //output
             //var clothing = Console.ReadLine(); //input
 
             //Console.WriteLine("What is a number between 10-40?"); //output
-            //var days = Console.ReadLine(); //input
+            //var numDays = Console.ReadLine(); //input
 
             //Console.WriteLine("What is your favorite animal?"); //output
-            //var animal = Console.ReadLine(); //input
+            //var faveAnimal = Console.ReadLine(); //input
 
             //Console.WriteLine("What is a number between 1-10?"); //output
-            //var treats = Console.ReadLine(); //input
+            //var numTreats = Console.ReadLine(); //input
 
-            //Console.WriteLine($"{name} had a lucky {color} {clothing}. " +
-            //    $"So lucky, the {clothing} hasn't been washed in {days} days." +
-            //    $" One day, their pet {animal} got hold of it. " +
-            //    $"After {treats} treats, their pet {animal} fell asleep. " +
-            //    $"Upon further investigation, {name} realized they were wearing it all along.");
+            //Console.WriteLine($"{firstName} had a lucky {faveColor} {clothing}. " +
+            //    $"So lucky, the {clothing} hasn't been washed in {numDays} days." +
+            //    $" One day, their pet {faveAnimal} got hold of it. " +
+            //    $"After {numTreats} treats, their pet {faveAnimal} fell asleep. " +
+            //    $"Upon further investigation, {firstName} realized they were wearing it all along.");
 
-            var sumAnswer = Add(2, 4);
+            var sumAnswer = Add(num 1, num2);           
             Console.WriteLine(sumAnswer);
 
             var productAnswer = Multiply(10, 2);
@@ -44,9 +44,13 @@ namespace MethodsExercise
         // Exercise 2:
         public static double Add(int num1, int num2)
         {
+           
+
             double sum = num1 + num2;
 
             return sum;
+
+
         }
 
         public static double Multiply(int num1, int num2)
